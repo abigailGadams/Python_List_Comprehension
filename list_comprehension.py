c220c729568727b2744d6b2faa2fb1517370bc1a
Abigail Adams <abigailgadams0@gmail.com>
2021-10-25 21:42:27 -0700

    conditional statements example

--- list_comprehension.py
+++ list_comprehension.py
@@ -7,6 +7,12 @@ number_list = [x for x in range(20) if x % 2 ==0]
 print(number_list)
 
 # Find all numbers that can be DIVIDED BY 2 AND 5
-
-num_list = [x for  x in range(20) if x %2 == 0]
+num_list = [y for  y in range(100) if (y %2 == 0) and (y % 5 ==0)]
 print(num_list)
+
+# Conditional Statements - iterating to find the even and odds from a list
+obj =  ["Even" if i%2==0 else "Odd" for i in range(10)]
+print(obj)
+
+# Creating Tuples 
+
